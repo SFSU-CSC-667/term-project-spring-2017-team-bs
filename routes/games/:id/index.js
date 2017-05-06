fe7f1eaf9eb88c04c7264abb55ffeafaf964cb16
stevielum1 <stevielum1@gmail.com>
2017-05-06 00:58:20 -0700

    removed users model in /routes/games/:id/index.js

--- routes/games/:id/index.js
+++ routes/games/:id/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const users = require('../../../models/users.js');
-
 router.get('/', function(req, res, next) {
   if(!req.user){
     req.flash('error', 'Redirected from /games/#, please use the create game and join game buttons');
